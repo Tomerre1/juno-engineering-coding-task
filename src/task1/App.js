b9de6f730fc32606212e03d2e4b20827b9b05ce9
Tomer Revah <revahtomer@gmail.com>
2022-05-05 16:59:33 +0300

    done first task carousel - styling and logic

--- src/task1/App.js
+++ src/task1/App.js
@@ -1,12 +1,11 @@
-import logo from '../logo.svg';
 import './App.css';
 import ImageCarousel from "./ImageCarousel";
 
 function App() {
   return (
-      <div>
-        <ImageCarousel />
-      </div>
+    <main className="flex center">
+      <ImageCarousel />
+    </main>
   );
 }
 
--- src/task1/ImageCarousel.js
+++ src/task1/ImageCarousel.js
@@ -1,7 +1,57 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { fetchImageUrls } from "../api/index";
+import { Loader } from './cmps/Loader'
+import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 
-const ImageCarousel = (props) => {
-    return <h1>Your code goes here</h1>;
-};
+const ImageCarousel = () => {
+    const [images, setImages] = useState([]);
+    const [currentImageIndex, setCurrentImageIndex] = useState(0);
+    const [isLoading, setIsLoading] = useState(false);
+
+    useEffect(() => {
+        getImagesUrls();
+    }, [])
+
+    const getImagesUrls = async () => {
+        try {
+            setIsLoading(true);
+            const fetchedImages = await fetchImageUrls();
+            setImages(fetchedImages);
+        } catch (err) {
+            console.log(`something worng with images fetching, check getImagesUrls function: ${err}`);
+        } finally {
+            setIsLoading(false);
+        }
+    }
+
+    const carouselNavigation = (num) => {
+        const nextIndex = currentImageIndex + num
+        const isIndexToHigh = nextIndex > images.length - 1;
+        const isIndexToLow = nextIndex < 0;
+
+        if (isIndexToHigh || isIndexToLow) {
+            const index = (isIndexToHigh) ? 0 : images.length - 1;
+            setCurrentImageIndex(index)
+        } else {
+            setCurrentImageIndex(currentImageIndex + num);
+        }
+    }
+
+    return (
+        <section className="container">
+            {isLoading ? <Loader />
+                :
+                <div className="carousel flex center">
+                    <ArrowBackIosNewIcon onClick={() => { carouselNavigation(1) }} />
+                    <img
+                        src={images[currentImageIndex]}
+                        alt="carousel img"
+                    />
+                    <ArrowForwardIosIcon onClick={() => { carouselNavigation(-1) }} />
+                </div>
+            }
+        </section >
+    );
+}
 export default ImageCarousel;
