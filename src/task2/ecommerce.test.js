9e6cc98ef003dcdf285c3c5ffa29ba6d9d4c7f0b
Tomer Revah <revahtomer@gmail.com>
2022-05-05 18:15:26 +0300

    task 2 done with tests

--- src/task2/ecommerce.test.js
+++ src/task2/ecommerce.test.js
@@ -1,7 +1,34 @@
-import { fetchOrderById } from "../api";
+import { fetchOrderById } from '../api';
+import { fetchAllOrders, bucketOrdersByUsers, getLast2WeeksOrders, bucketOrdersByDate } from './ecommerce';
 
 const ORDER_ID = "70ef599e5eca171b2bce84d1"
-test("Ecommerce - fetchOrderById", async () => {
-    let orders = await fetchOrderById(ORDER_ID);
+
+test('Ecommerce - fetchOrderById', async () => {
+    const orders = await fetchOrderById(ORDER_ID);
+    console.log(orders);
     expect(orders).toBeTruthy();
+});
+
+test('Ecommerce - fetchAllOrders', async () => {
+    const allOrders = await fetchAllOrders();
+    console.log(allOrders);
+    expect(allOrders).toBeTruthy();
+});
+
+it('Ecommerce - bucketOrdersByUsers', async () => {
+    let orderByUsers = await bucketOrdersByUsers();
+    console.log(orderByUsers);
+    expect(orderByUsers).toBeTruthy();
+});
+
+it('Ecommerce - getLast2WeeksOrders', async () => {
+    let twoWeeksAgo = await getLast2WeeksOrders();
+    console.log(twoWeeksAgo);
+    expect(twoWeeksAgo).toBeTruthy();
+});
+
+it('Ecommerce - bucketOrdersByDate', async () => {
+    let orderByDate = await bucketOrdersByDate();
+    console.log(orderByDate);
+    expect(orderByDate).toBeTruthy();
 });
